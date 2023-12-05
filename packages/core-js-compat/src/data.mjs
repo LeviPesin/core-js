95c4e785742de9e81fa01141eb868e3d64edf5f4
Denis Pushkarev <zloirock@zloirock.ru>
2023-12-05 22:49:31 +0700

    drop `Function.{ isCallable, isConstructor}` (proposal is dead)

--- packages/core-js-compat/src/data.mjs
+++ packages/core-js-compat/src/data.mjs
@@ -2101,10 +2101,6 @@ export const data = {
   },
   'esnext.function.demethodize': {
   },
-  'esnext.function.is-callable': {
-  },
-  'esnext.function.is-constructor': {
-  },
   'esnext.function.metadata': {
   },
   // TODO: Remove from `core-js@4`
