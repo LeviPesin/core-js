536a57a02eed6147c68f0c2ffbc2f08182a67a53
Denis Pushkarev <zloirock@zloirock.ru>
2024-02-08 09:27:25 +0700

    drop legacy entries for `ArrayBuffer#transfer` proposal

--- packages/core-js-compat/src/data.mjs
+++ packages/core-js-compat/src/data.mjs
@@ -2051,12 +2051,6 @@ export const data = {
   },
   'esnext.array.unique-by': {
   },
-  // TODO: Remove from `core-js@4`
-  'esnext.array-buffer.detached': null,
-  // TODO: Remove from `core-js@4`
-  'esnext.array-buffer.transfer': null,
-  // TODO: Remove from `core-js@4`
-  'esnext.array-buffer.transfer-to-fixed-length': null,
   'esnext.async-disposable-stack.constructor': {
   },
   'esnext.async-iterator.constructor': {
@@ -2465,9 +2459,6 @@ export const data = {
 
 export const renamed = new Map([
   // TODO: Clean in `core-js@4`
-  ['esnext.array-buffer.detached', 'es.array-buffer.detached'],
-  ['esnext.array-buffer.transfer', 'es.array-buffer.transfer'],
-  ['esnext.array-buffer.transfer-to-fixed-length', 'es.array-buffer.transfer-to-fixed-length'],
   ['esnext.iterator.constructor', 'es.iterator.constructor'],
   ['esnext.iterator.drop', 'es.iterator.drop'],
   ['esnext.iterator.every', 'es.iterator.every'],
